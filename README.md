097459e23822f7bd4435fe999544b6b16a4f4e87
Mike Anderson <miketanderson@gmail.com>
2015-06-02 21:50:27 -0400

    basic cleanup, preparing for markdown inclusion

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # textlocker
 Create, edit, and view AES encrypted text based files
+
+/data/ folder requires RW permissions from your php process
+
+
--- resources/text_view.php
+++ resources/text_view.php
@@ -11,21 +11,11 @@ $new_file = false;
 #txte file type is txt encrypted
 $data_file = '../data/' . $hash . '.txte';
 
-if (isset($_POST['create'])) {
-  $file = fopen($data_file, "w") or die("Unable to open file!");
-  fclose($file);
-}
-
-if (isset($_POST['textform'])) {
-  set_text($_POST['textform'], $data_file, $key);  
-}
-
 if (file_exists($data_file) ) {
   #open file
   $text = get_text($data_file, $key); 
 } else {
-  #flag to create file  
-  $new_file = true; 
+  $error = 'There is no data file for this user yet.';
 }
 
 ?>
@@ -40,22 +30,11 @@ if (file_exists($data_file) ) {
 <p>
 <?php 
 #echo $key;
-if ($new_file) {
-  print '<p>It looks like you are missing a data file, click to create one</p>';
-  print '<form action="text_view.php" method="post">';
-  print '<input name="create" type="submit" value="Create File">';
-
-}
- else {
-  print 'Text Input<p><div id="text_edit">';
-  print '<form action="text_view.php" method="post">';
-  print '<textarea rows="10" cols="75" name="textform">';
+ 
+  print '<p><div id="text_view">';
   print $text;
-  print '</textarea>';
-  print '</p><p>';
-  print '<input name="submit" type="submit" value="Save">';
-  print '</form></div></p>';
-}
+  print '</p>';
+
  ?>
 </p>
 <?php require_once('footer.php') ?>
