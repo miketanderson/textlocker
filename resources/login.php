1a5409a366839d9e320768234d3c6dbb5b5104fe
Mike Anderson <miketanderson@gmail.com>
2015-06-03 21:13:39 -0400

    explicitly verify passwords

--- resources/login.php
+++ resources/login.php
@@ -23,13 +23,25 @@ if (isset($_POST['submit'])) {
     $user_hash = hash('sha256',$pepper . $username);
     #if exist file = user_hash, login = true
     $file_salt = '../data/' . $user_hash . '.salt';
+    $file_pass = '../data/' . $user_hash . '.chk';
     if (file_exists($file_salt) ) {
-      $_SESSION['user_name']=$username;
-      $salt = file_get_contents($file_salt); 
-      $key = hash_password($password,$salt);
-      $_SESSION['user_key']=$key;
-      $_SESSION['user_hash']=$user_hash;
-      header("location: text_view.php");
+        #TODO: check password vs hash here
+        if (file_exists($file_pass)) {
+          $_SESSION['user_name']=$username;
+          $salt = file_get_contents($file_salt); 
+          $key = hash_password($password,$salt);
+          #Use encrypted key, decrypt .chk file, validate file contents = "TRUE"
+          $passchk = file_get_contents($file_pass);
+          $decchk = aes_decrypt($key, $passchk); 
+          if ($decchk === "TRUE") {
+            $_SESSION['user_key']=$key;
+            $_SESSION['user_hash']=$user_hash;
+            header("location: text_view.php");
+          }
+          else {
+              $error = "Username or password is invalid";
+          }
+        }
 
     }
     else {
@@ -42,14 +54,27 @@ if (isset($_POST['submit'])) {
         fclose($file);
         $_SESSION['user_name']=$username;
         $key = hash_password($password,$salt);
-        $_SESSION['user_key']=$key;
-        $_SESSION['user_hash']=$user_hash;
-        header("location: text_view.php");
+        
+        #Use encrypted key, create user_hash.ckc file, encrypt($key, "TRUE") save to file
+        if (file_exists($file_pass)) {
+            #user already exists but new user flag is set, don't proceed
+          $error = "Can't create user";
+        }
+        else {
+
+          $fpass = fopen($file_pass, "w") or die("Unable to open file!");
+          $encchk = aes_encrypt($key,"TRUE");
+          fwrite($fpass,$encchk);
+          fclose($fpass);
+
+          $_SESSION['user_key']=$key;
+          $_SESSION['user_hash']=$user_hash;
+          header("location: text_view.php");
+        }
 
       } else {
         $error = "Username or password is invalid";
       }
-      #uncomment to create new user
       #$error = $file_salt . " not found";
     }
   }
